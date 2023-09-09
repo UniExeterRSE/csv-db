6a3bff6bec0a817689e6f786947948e711cd1a2a
Thomas Hawes <105276609+thawes-rse@users.noreply.github.com>
2023-09-09 19:59:08 +0100

    Move db creation to fixture set up

--- tests/test_core.py
+++ tests/test_core.py
@@ -8,36 +8,34 @@ class TestCsvDB(unittest.TestCase):
         self.file_name = "db.csv"
         self.fields = ["id", "col1"]
         self.pkey = "id"
+        self.db = CsvDB(self.file_name, self.fields)
         self.record = {self.pkey: "1", "col1": "a"}
 
     def test_create_retrieve(self):
         """Test that a record can be retrieved based on the value of a column."""
 
-        with CsvDB(self.file_name, self.fields) as db:
-            db.create(self.record)
-            self.assertEqual(self.record, db.retrieve(self.record[self.pkey], self.pkey))
+        self.db.create(self.record)
+        self.assertEqual(self.record, self.db.retrieve(self.record[self.pkey], self.pkey))
 
     def test_create_take_non_string_values(self):
         """Test that records can be created and retrieved based on non-string
         values."""
 
-        with CsvDB(self.file_name, self.fields) as db:
-            record = {self.pkey: 1, "col1": 2}
-            db.create(record)
-            self.assertEqual(
-                {self.pkey: "1", "col1": "2"}, db.retrieve(record[self.pkey], self.pkey)
-            )
+        record = {self.pkey: 1, "col1": 2}
+        self.db.create(record)
+        self.assertEqual(
+            {self.pkey: "1", "col1": "2"}, self.db.retrieve(record[self.pkey], self.pkey)
+        )
 
     def test_create_error_missing_field(self):
         """Test that a ValueError is raised if the record submitted for creation has
         a field missing."""
 
-        with CsvDB(self.file_name, self.fields) as db:
-            record = {self.pkey: "1"}
-            with self.assertRaisesRegex(
-                ValueError, "^Argument 'record' missing the following fields: 'col1'.$"
-            ):
-                db.create(record)
+        record = {self.pkey: "1"}
+        with self.assertRaisesRegex(
+            ValueError, "^Argument 'record' missing the following fields: 'col1'.$"
+        ):
+            self.db.create(record)
 
     def test_create_error_missing_fields_multiple(self):
         """Test that a ValueError is raised if the record submitted for creation has
