c77613549d3c5c3b7ffcd5c4ca44f6cbaff93503
Thomas Hawes <105276609+thawes-rse@users.noreply.github.com>
2023-09-09 17:44:51 +0100

    Add validation to record fields on creation

--- src/csv_db/core.py
+++ src/csv_db/core.py
@@ -1,11 +1,13 @@
 import contextlib
+from collections.abc import Collection
 from types import TracebackType
 from typing import Any
 
 
 class CsvDB(contextlib.AbstractContextManager):
-    def __init__(self, path: str):
+    def __init__(self, path: str, fields: Collection[str]):
         self._records = []
+        self._fields = fields
 
     def __enter__(self):
         return super().__enter__()
@@ -19,6 +21,12 @@ class CsvDB(contextlib.AbstractContextManager):
         return super().__exit__(__exc_type, __exc_value, __traceback)
 
     def create(self, record: dict[str, Any]):
+        missing_fields = ", ".join([f"'{k}'" for k in self._fields if k not in record])
+        if missing_fields:
+            raise ValueError(
+                f"Argument 'record' missing the following fields: {missing_fields}."
+            )
+
         self._records.append({k: str(v) for k, v in record.items()})
 
     def retrieve(self, value: Any, field: str):
