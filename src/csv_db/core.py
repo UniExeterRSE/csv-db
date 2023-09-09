e2b8c904ff9710c203e46213c876b095124167d4
Thomas Hawes <105276609+thawes-rse@users.noreply.github.com>
2023-09-09 16:51:24 +0100

    Start methods to create and retrieve methods

--- src/csv_db/core.py
+++ src/csv_db/core.py
@@ -1,2 +1,25 @@
-def hello():
-    return "Hello"
+import contextlib
+from types import TracebackType
+from typing import Any
+
+
+class CsvDB(contextlib.AbstractContextManager):
+    def __init__(self, path: str):
+        self._records = []
+
+    def __enter__(self):
+        return super().__enter__()
+
+    def __exit__(
+        self,
+        __exc_type: type[BaseException] | None,
+        __exc_value: BaseException | None,
+        __traceback: TracebackType | None,
+    ) -> bool | None:
+        return super().__exit__(__exc_type, __exc_value, __traceback)
+
+    def create(self, record: dict[str, Any]):
+        self._records.append(record)
+
+    def retrieve(self, value: Any, field: str):
+        return self._records[0]
--- tests/test_core.py
+++ tests/test_core.py
@@ -1,11 +1,17 @@
 import unittest
 
-from csv_db.core import hello
+from csv_db.core import CsvDB
 
 
-class TestHello(unittest.TestCase):
-    def test_hello(self):
-        self.assertEqual("Hello", hello())
+class TestCsvDB(unittest.TestCase):
+    def test_create_retrieve(self):
+        """Test that a record can be retrieved based on the value of a column."""
+
+        with CsvDB("db.csv") as db:
+            field = "id"
+            record = {field: "1"}
+            db.create(record)
+            self.assertEqual(record, db.retrieve(record[field], field))
 
 
 if __name__ == "__main__":
