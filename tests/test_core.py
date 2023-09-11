88d2005f6766fe097cbd8e9d281db9b8f058aa05
Thomas Hawes <105276609+thawes-rse@users.noreply.github.com>
2023-09-11 11:23:17 +0100

    Refactor tests

--- tests/test_core.py
+++ tests/test_core.py
@@ -1,6 +1,7 @@
 import os
 import tempfile
 import unittest
+from typing import Iterable
 
 from csv_db.core import CsvDB, DatabaseLookupError, FieldsMismatchError
 
@@ -16,20 +17,30 @@ def exact(string: str):
     return "^" + escaped + "$"
 
 
+def write_csv_row(path, data: Iterable[str], mode="x"):
+    """Write data to a csv file as a comma-separated row."""
+    with open(path, mode=mode, newline="") as f:
+        f.write(",".join(data) + "\n")
+
+
 class TestCsvDB(unittest.TestCase):
     def setUp(self) -> None:
+        # Temp directory for holding csv file during a unit test run
         self._dir = tempfile.TemporaryDirectory()
         self.tmp_dir = self._dir.name
+
+        # An empty database with fields and record to add to it
         self.path = os.path.join(self.tmp_dir, "db.csv")
-        self.fields = ["id", "col1"]
         self.pkey = "id"
+        self.col1 = "col1"
+        self.fields = [self.pkey, self.col1]
         self.db = CsvDB(self.path, self.fields)
-        self.record = {self.pkey: "1", "col1": "a"}
+        self.record = {self.pkey: "1", self.col1: "a"}
 
         # Database with two records in it
         self.path2 = os.path.join(self.tmp_dir, "db2.csv")
         self.db2 = CsvDB(self.path2, self.fields)
-        self.record2 = {self.pkey: "2", "col1": "b"}
+        self.record2 = {self.pkey: "2", self.col1: "b"}
         self.db2.create(self.record)
         self.db2.create(self.record2)
 
@@ -40,18 +51,14 @@ class TestCsvDB(unittest.TestCase):
         """Test that a database can be initialised on an existing csv file having
         the correct fields."""
 
-        with open(self.path, mode="x", newline="") as f:
-            f.write(",".join(self.fields) + "\n")
-
+        write_csv_row(self.path, self.fields)
         _ = CsvDB(self.path, self.fields)
 
     def test_initialise_existing_file_fields_have_diff_order(self):
         """Test that a database can be initialised on an existing csv file having
         the correct fields but in a different order to those supplied."""
 
-        with open(self.path, mode="x", newline="") as f:
-            f.write(",".join(reversed(self.fields)) + "\n")
-
+        write_csv_row(self.path, reversed(self.fields))
         _ = CsvDB(self.path, self.fields)
 
     @unittest.skip("Not yet implemented")
@@ -70,9 +77,7 @@ class TestCsvDB(unittest.TestCase):
         """Test that an FieldsMismatchError is raised if a database is initialised on
         an existing csv file that has a different header to the one specified."""
 
-        with open(self.path, mode="x", newline="") as f:
-            f.write("a,b\n")
-
+        write_csv_row(self.path, ["a", "b"])
         with self.assertRaisesRegex(
             FieldsMismatchError,
             exact(f"'fields' does not agree with the fields defined in {self.path}"),
@@ -98,10 +103,11 @@ class TestCsvDB(unittest.TestCase):
         """Test that records can be created and retrieved based on non-string
         values."""
 
-        record = {self.pkey: 1, "col1": 2}
+        record = {self.pkey: 1, self.col1: 2}
         self.db.create(record)
         self.assertEqual(
-            {self.pkey: "1", "col1": "2"}, self.db.retrieve(record[self.pkey], self.pkey)
+            {self.pkey: "1", self.col1: "2"},
+            self.db.retrieve(record[self.pkey], self.pkey),
         )
 
     def test_create_error_missing_field(self):
@@ -128,7 +134,7 @@ class TestCsvDB(unittest.TestCase):
     def test_create_multiple_records(self):
         """Test that multiple records can be added to the database sequentially."""
 
-        record2 = {self.pkey: "2", "col1": "b"}
+        record2 = {self.pkey: "2", self.col1: "b"}
         self.db.create(self.record)
         self.db.create(record2)
         for record in [self.record, record2]:
@@ -138,14 +144,14 @@ class TestCsvDB(unittest.TestCase):
         """Test that the csv file has the expected content when multple records are
         created sequentially."""
 
-        record2 = {self.pkey: "2", "col1": "b"}
+        record2 = {self.pkey: "2", self.col1: "b"}
         self.db.create(self.record)
         self.db.create(record2)
         with open(self.path, mode="r", newline=None) as csvfile:
             expected = (
                 [",".join(self.fields) + "\n"]
-                + [",".join([self.record[self.pkey], self.record["col1"]]) + "\n"]
-                + [",".join([record2[self.pkey], record2["col1"]]) + "\n"]
+                + [",".join([self.record[self.pkey], self.record[self.col1]]) + "\n"]
+                + [",".join([record2[self.pkey], record2[self.col1]]) + "\n"]
             )
             self.assertEqual(expected, csvfile.readlines())
 
@@ -158,7 +164,7 @@ class TestCsvDB(unittest.TestCase):
 
         # Create new database from same file and add record
         db = CsvDB(self.path, self.fields)
-        record2 = {self.pkey: "2", "col1": "b"}
+        record2 = {self.pkey: "2", self.col1: "b"}
         db.create(record2)
 
         self.assertEqual(self.record, db.retrieve(self.record[self.pkey], self.pkey))
@@ -168,9 +174,7 @@ class TestCsvDB(unittest.TestCase):
         """Test that a record can be added to a database in which the fields are in a
         different order to those supplied at initialisation."""
 
-        with open(self.path, mode="x", newline="") as f:
-            f.write(",".join(self.fields) + "\n")
-
+        write_csv_row(self.path, self.fields)
         db = CsvDB(self.path, list(reversed(self.fields)))
         db.create(self.record)
         self.assertEqual(self.record, db.retrieve(self.record[self.pkey], self.pkey))
@@ -180,7 +184,7 @@ class TestCsvDB(unittest.TestCase):
         records get updated."""
 
         lookup_val = self.record2[self.pkey]
-        updated_record = {self.pkey: lookup_val, "col1": "c"}
+        updated_record = {self.pkey: lookup_val, self.col1: "c"}
         self.db2.update(lookup_val, self.pkey, updated_record)
         self.assertEqual(updated_record, self.db2.retrieve(lookup_val, self.pkey))
         self.assertEqual(
@@ -191,8 +195,8 @@ class TestCsvDB(unittest.TestCase):
         """Test that the record with the specified field value gets updated after
         converting the value to a string."""
 
-        self.db.create({self.pkey: "1", "col1": "a"})
-        new_record = {self.pkey: "1", "col1": "b"}
+        self.db.create(self.record)
+        new_record = {self.pkey: "1", self.col1: "b"}
         self.db.update(1, self.pkey, new_record)
         self.assertEqual(new_record, self.db.retrieve(new_record[self.pkey], self.pkey))
 
